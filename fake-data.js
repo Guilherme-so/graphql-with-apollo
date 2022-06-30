f43aa27c6dc2d17f41f28597f96574a466bd802b
Guilherme013S <guilhermemagno09@gmail.com>
2022-06-30 15:50:52 -0300

    using graphql with apollo on server side

---
+++ fake-data.js
@@ -0,0 +1,94 @@
+const userList = [
+    {
+        id: 1,
+        name: "Monark",
+        username: "Bud",
+        age: 36,
+        nationality: "CANADA",
+        friends: [
+            {
+                id: 4,
+                name: "Bolsonaro",
+                username: "Messias",
+                age: 68,
+                nationality: "BRASIL"
+            },
+            {
+                id: 2,
+                name: "Chris",
+                username: "Suco de frutas",
+                age: 16,
+                nationality: "GERMANY"
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "Chris",
+        username: "Suco de frutas",
+        age: 16,
+        nationality: "GERMANY"
+    },
+    {
+        id: 3,
+        name: "Erdogan",
+        username: "Demogorgon",
+        age: 36,
+        nationality: "TURKEY"
+    },
+    {
+        id: 4,
+        name: "Bolsonaro",
+        username: "Messias",
+        age: 68,
+        nationality: "BRASIL"
+    },
+];
+
+const movieList = [
+    {
+        id: 1,
+        name: "Shazam",
+        rate: 87,
+        releseadYear: 2019,
+        isInTheaters: true,
+    },
+    {
+        id: 2,
+        name: "Morbius",
+        rate: 7,
+        releseadYear: 2022,
+        isInTheaters: true,
+    },
+    {
+        id: 3,
+        name: "Luca",
+        rate: 83,
+        releseadYear: 2021,
+        isInTheaters: true,
+    },
+    {
+        id: 4,
+        name: "Free Guy",
+        rate: 82,
+        releseadYear: 2021,
+        isInTheaters: true,
+    },
+    {
+        id: 5,
+        name: "Uncharted",
+        rate: 90,
+        releseadYear: 2021,
+        isInTheaters: true,
+    },
+    {
+        id: 6,
+        name: "Guis pursuit of happyness",
+        releseadYear: 2030,
+        isInTheaters: false,
+    },
+
+]
+
+
+module.exports = { userList, movieList }
---
+++ schema/resolvers.js
@@ -0,0 +1,59 @@
+const { userList, movieList } = require("../fake-data")
+const _ = require("lodash")
+
+const resolvers = {
+    Query: {
+        // Users resolvers
+        users: () => {
+            return userList
+        },
+        user: (parent, args) => {
+            const id = args.id
+            const user = userList.find((single) => single.id === Number(id))
+            return user
+        },
+        //Movies resolvers
+        movies: () => {
+            return movieList
+        },
+        movie: (parent, args) => {
+            const name = args.name
+            const movie = movieList.find((movie) => movie.name === name)
+            return movie
+        }
+    },
+    User: {
+        favoriteMovies: () => {
+            return movieList.filter((lista) => lista.rate >= 80)
+        }
+    },
+    Mutation: {
+        createUser: (parent, args) => {
+            const user = args.input
+            const lastId = userList[userList.length - 1].id
+            user.id = lastId + 1
+            userList.push(user)
+            return user
+        },
+        updateUsername: (parent, args) => {
+            const { id, newUsername } = args.input
+            let updatedUser;
+
+            userList.forEach((user) => {
+                if (user.id === Number(id)) {
+                    user.username = newUsername
+                    updatedUser = user
+                }
+            })
+            return updatedUser
+        },
+        deleteUser: (parent, args) => {
+            const id = args.id
+            const formatedId = Number(id)
+            _.remove(userList, (user) => user.id === formatedId)
+            return null
+        }
+    }
+}
+
+module.exports = { resolvers }
